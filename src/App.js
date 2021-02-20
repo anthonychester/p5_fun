7d7f2ab9991cc0b619e0fa5ac6aca9fe9a686896
anthonychester <60993155+anthonychester@users.noreply.github.com>
2021-02-20 12:15:29 -0600

    I flash in progress, console log working

--- src/App.js
+++ src/App.js
@@ -3,23 +3,76 @@ import Sketch from "react-p5";
 
 export default class App extends Component {
   setup = (p5, canvasParentRef) => {
+    this.cont = 0;
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(
       canvasParentRef
     );
     p5.frameRate(this.fr);
     // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
   };
+
+  renderSquare = i => {
+    //console.log(i);
+    //return <p>i</p>;
+  }
+
   draw = p5 => {
-    p5.fill(234, 31, 81);
+    //p5.fill(234, 31, 81);
+    if (this.cont === 5 && this.cont) {
+      this.cont = 0;
+      if (this.color === "#000") {
+        this.color = "#555";
+      } else {
+        this.color = "#000";
+      }
+    }
+    this.color = "#000";
+    p5.fill(this.color);
     p5.noStroke();
     p5.rect(50, 50, 250, 250);
     p5.fill(255);
     p5.textStyle(p5.BOLD);
     p5.textSize(140);
     p5.text("p5*", 60, 250);
+
+    this.cont = this.cont + 1;
+    //console.log(this.cont);
+    this.renderSquare(this.cont)
   };
 
+  Square = Square => {
+      return (
+        <button className="square">
+          {/* TODO */}
+        </button>
+      );
+    
+  }
+
+  renderSquare(i) {
+    return <this.Square />;
+  }
+
+  Game = Game => {
+
+
+      return (
+        <div className="game">
+          <div className="game-board">
+            <renderSquare />
+          </div>
+          <div className="game-info">
+            <div>{/* status */}</div>
+            <ol>{/* TODO */}</ol>
+          </div>
+        </div>
+      );
+    
+      
+  }
+
   render() {
-    return <Sketch setup={this.setup} draw={this.draw} />;
+    return <this.Game />, <Sketch setup={this.setup} draw={this.draw} />;
+    
   }
 }

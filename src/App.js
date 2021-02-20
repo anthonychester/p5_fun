494d9286175636167d1cef36e4797da44b9178e9
anthonychester <60993155+anthonychester@users.noreply.github.com>
2021-02-20 12:34:16 -0600

    III made screen flash between black and white for text and set conter to 30

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Sketch from "react-p5";
 export default class App extends Component {
   setup = (p5, canvasParentRef) => {
     this.cont = 0;
+    this.color_back = "#fff";
+    this.color_text = "#000";
     p5.createCanvas(window.innerWidth, window.innerHeight).parent(
       canvasParentRef
     );
@@ -11,26 +13,27 @@ export default class App extends Component {
     // use parent to render canvas in this ref (without that p5 render this canvas outside your component)
   };
 
-  renderSquare = i => {
+  renderSquare = (i) => {
     //console.log(i);
     //return <p>i</p>;
-  }
+  };
 
-  draw = p5 => {
+  draw = (p5) => {
     //p5.fill(234, 31, 81);
-    if (this.cont === 5 && this.cont) {
-      this.cont = 0;
-      if (this.color === "#000") {
-        this.color = "#555";
+    if (this.cont === 30) {
+      if (this.color_back === "#000") {
+        this.color_back = "#fff";
+        this.color_text = "#000";
       } else {
-        this.color = "#000";
+        this.color_back = "#000";
+        this.color_text = "#fff";
       }
+      this.cont = 0;
     }
-    this.color = "#000";
-    p5.fill(this.color);
+    p5.fill(this.color_back);
     p5.noStroke();
     p5.rect(50, 50, 250, 250);
-    p5.fill(255);
+    p5.fill(this.color_text);
     p5.textStyle(p5.BOLD);
     p5.textSize(140);
     p5.text("p5*", 60, 250);
@@ -40,42 +43,32 @@ export default class App extends Component {
 
     this.cont = this.cont + 1;
     //console.log(this.cont);
-    this.renderSquare(this.cont)
+    this.renderSquare(this.cont);
   };
 
-  Square = Square => {
-      return (
-        <button className="square">
-          {/* TODO */}
-        </button>
-      );
-    
-  }
+  Square = (Square) => {
+    return <button className="square">{/* TODO */}</button>;
+  };
 
   renderSquare(i) {
     return <this.Square />;
   }
 
-  Game = Game => {
-
-
-      return (
-        <div className="game">
-          <div className="game-board">
-            <renderSquare />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+  Game = (Game) => {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <renderSquare />
         </div>
-      );
-    
-      
-  }
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  };
 
   render() {
-    return <this.Game />, <Sketch setup={this.setup} draw={this.draw} />;
-    
+    return (<this.Game />), (<Sketch setup={this.setup} draw={this.draw} />);
   }
 }
